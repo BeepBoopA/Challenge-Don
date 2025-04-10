900318eaf4b34eb4c67a8dc3eff060065eabbaf3
CatSand <aarontheskeleton@gmail.com>
2025-04-11 02:02:29 +1000

    Initialising bot (so it works)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world")
+import 'dotenv/config';
+import { Client } from 'discord.js';
+
+const client = new Client({
+    intents: [
+        'Guilds',
+        'GuildMessages'
+    ],
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
