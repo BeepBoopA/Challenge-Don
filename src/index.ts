1b2b41d0e17452e5a10a3f1e8901412e8071acfe
CatSand <aarontheskeleton@gmail.com>
2025-04-14 01:46:33 +1000

    Random :sad: react 10% chance

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, GuildEmoji } from 'discord.js';
 
 const client = new Client({
     intents: [
@@ -11,3 +11,10 @@ const client = new Client({
 
 client.login(process.env.DISCORD_TOKEN);
 
+client.on("messageCreate", (message) => {
+    const random = Math.random() * 9 + 1;
+
+    if (random >= 9) {
+        message.react('735674877243031593');
+    }
+});
