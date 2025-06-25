826aefba2540b849867564dd672b722e40ac7417
BeepBoopA <aaronlay.10lch@gmail.com>
2025-06-25 19:52:45 +1000

    Created event folder

--- src/index.js
+++ src/index.js
@@ -12,16 +12,19 @@ const client = new Client({
     ]
 });
 
-// File Setup
+// Folder Scan Setup
 client.commands = new Collection();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Read Commands Folder
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
         const fileURL = pathToFileURL(filePath).href;
@@ -35,10 +38,24 @@ for (const folder of commandFolders) {
 	}
 }
 
-// Run on Client Ready
-client.once(Events.ClientReady, ready => {
-    console.log(`Ready! Logged in as ${ready.user.tag}`)
-});
+// Read Events Folder
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const fileURL = pathToFileURL(filePath).href;
+    const { default: event } = await import(fileURL);
+
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    }
+    else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+// Dont delete this again please
 client.login(process.env.DISCORD_TOKEN);
 
 // Command Interactions
