f0f8ad472ec12b7a20d3fb68cc602beeb59bac33
BeepBoopA <aaronlay.10lch@gmail.com>
2025-06-26 00:47:12 +1000

    move interaction create to events folder

--- src/index.js
+++ src/index.js
@@ -57,25 +57,3 @@ for (const file of eventFiles) {
 
 // Dont delete this again please
 client.login(process.env.DISCORD_TOKEN);
-
-// Command Interactions
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    
-    const command = interaction.client.commands.get(interaction.commandName);
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    }
-    catch (e) {
-        if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		}
-    }
-})
