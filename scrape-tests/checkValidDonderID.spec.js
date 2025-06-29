45c7eee295236758ccb45cf07ac83a1c4bb10b31
BeepBoopA <aaronlay.10lch@gmail.com>
2025-06-29 23:39:16 +1000

    implemented donderExists function

--- scrape-tests/checkValidDonderID.spec.js
+++ scrape-tests/checkValidDonderID.spec.js
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test';
 import 'dotenv/config'
 
-test('DonderExists', async ({ page }) => {
+test('DonderExistsTrue', async ({ page }) => {
   await page.goto('https://donderhiroba.jp/login.php');
   await page.getByRole('img', { name: 'ログイン' }).click();
 
@@ -15,4 +15,33 @@ test('DonderExists', async ({ page }) => {
   // Front End Validation + Login
   await page.keyboard.press('Tab');
   await page.locator('#btn-idpw-login').click();
+
+  await page.getByText('ログイン').click();
+  await page.goto('https://donderhiroba.jp/user_search.php?exec=1&keyword=594288502311');
+
+  const donderExists = await page.locator('.friendArea.clearfix').isVisible();
+
+  expect(donderExists).toBeTruthy();
+});
+
+test('DonderExistsFalse', async ({ page }) => {
+  await page.goto('https://donderhiroba.jp/login.php');
+  await page.getByRole('img', { name: 'ログイン' }).click();
+
+  // Fill Email & Password
+  await page.locator('#mail')
+            .fill(process.env.MAIN_BOT_EMAIL || 'Empty Beans');
+  await page.locator('#pass')
+            .fill(process.env.MAIN_BOT_PASSWORD || 'Empty Cheese');
+
+  // Front End Validation + Login
+  await page.keyboard.press('Tab');
+  await page.locator('#btn-idpw-login').click();
+
+  await page.getByText('ログイン').click();
+  await page.goto('https://donderhiroba.jp/user_search.php?exec=1&keyword=');
+
+  const donderExists = await page.locator('.friendArea.clearfix').isVisible();
+
+  expect(donderExists).toBeFalsy();
 });
--- src/commands/modules/checkValidDonderID.js
+++ src/commands/modules/checkValidDonderID.js
@@ -4,4 +4,22 @@ const checkValidDonderID = async (donderID) => {
     const broswer = await firefox.launch();
     const page = broswer.newPage();
     await page.goto('https://donderhiroba.jp/login.php');
-}
+    await page.getByRole('img', { name: 'ログイン' }).click();
+
+    // Fill Email & Password
+    await page.locator('#mail')
+                .fill(process.env.MAIN_BOT_EMAIL || 'Empty Beans');
+    await page.locator('#pass')
+                .fill(process.env.MAIN_BOT_PASSWORD || 'Empty Cheese');
+
+    // Front End Validation + Login
+    await page.keyboard.press('Tab');
+    await page.locator('#btn-idpw-login').click();
+    await page.getByText('ログイン').click();
+
+    // Find donderID
+    await page.goto(`https://donderhiroba.jp/user_search.php?exec=1&keyword=${donderID}`);
+    const donderExists = await page.locator('.friendArea.clearfix').isVisible();
+
+    return donderExists;
+}   
