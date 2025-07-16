390b2fb7881ba285f3a67d8537e8ebcab0cf3aa9
BeepBoopA <aaronlay.10lch@gmail.com>
2025-07-16 20:13:06 +1000

    Modularised challenge creation (if thats the right word idk)

---
+++ src/modules/createChallenge.js
@@ -0,0 +1,39 @@
+import db from "../database/database.js";
+
+export const setChallenge = async (challengeName, song1, song2, song3) => {
+    // Insert Challenge
+    let insert = null;
+
+    insert = db.prepare("INSERT INTO challenges (name) VALUES (?)");
+    const challengeID = insert.run(`${challengeName}`).lastInsertRowid;
+
+    insert = db.prepare("INSERT INTO charts (name) VALUES (?)");
+    const songIDs = [
+        insert.run(`${song1}`).lastInsertRowid,
+        insert.run(`${song2}`).lastInsertRowid,
+        insert.run(`${song3}`).lastInsertRowid,
+    ];
+
+    insert = db.prepare(
+        "INSERT INTO challenge_charts (challenge_id, chart_id) VALUES (?, ?)"
+    );
+    for (let i = 0; i < songIDs.length; i++) {
+        insert.run(`${challengeID}`, `${songIDs[i]}`);
+    }
+
+    // Test:
+    let select = null;
+    let rows = null;
+
+    select = db.prepare("SELECT * FROM challenges");
+    rows = select.all();
+    console.log("Challenge data: ", rows);
+
+    select = db.prepare("SELECT * FROM charts");
+    rows = select.all();
+    console.log("Chart data: ", rows);
+
+    select = db.prepare("SELECT * FROM challenge_charts");
+    rows = select.all();
+    console.log("Challenge - Chart data: ", rows);
+};
