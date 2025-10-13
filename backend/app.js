a4b1a0b88233df6cf376f7a1f4db58b3e8ee315c
Aaron Lay <aaronlay.10lch@gmail.com>
2025-10-13 18:27:04 +1100

    Added routes to app

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import cors from 'cors';
-import userRoutes from './routes/users.js';
+import { userRoutes } from './routes/userRoute.js';
+import { challengeChartRoutes } from './routes/challengeChartRoute.js';
+import { challengeRoutes } from './routes/challengeRoute.js';
+import { chartRoutes } from './routes/chartRoute.js';
+import { scoreRoutes } from './routes/scoreRoute.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
-// app.use('/users', userRoutes());
+app.use('/users', userRoutes());
+app.use('/challengeCharts', challengeChartRoutes());
+app.use('/challenges', challengeRoutes());
+app.use('/charts', chartRoutes());
+app.use('/scores', scoreRoutes());
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:3000');
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import Database from 'better-sqlite3';
 
-const db = new Database('../data.db');
+const db = new Database('../data/data.db');
 
 // Tables - hopefully this doesnt break somehow...
 db.exec(`
